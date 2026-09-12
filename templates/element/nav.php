079c16202e82c2617821c1b2b86f7c2312459284
KaveZoo <kavezoo@gmail.com>
2026-09-12 08:17:08 +0200

    save

--- templates/element/nav.php
+++ templates/element/nav.php
@@ -3,8 +3,10 @@
               <li><a href="chart.html"><i class="fa-solid fa-chart-bar"></i>Charts</a></li>
               <li class="active has-sub"><a class="js-arrow open" href="#"><i class="fa-solid fa-table"></i>Tables</a>
                 <ul class="list-unstyled navbar__sub-list js-sub-list" style="display: block;">
-                  <li class="active"><a href="?m=index">Tables</a></li>
-                  <li><a href="?m=form">Form</a></li>
+                  <li class="active"><a href="/admin/customers">Customers</a></li>
+                  <li><a href="/admin/orders">Orders</a></li>
+                  <li><a href="/admin/items">Items</a></li>
+                  <li><a href="/admin/ordersItems">OrdersItems</a></li>
                 </ul>
               </li>
             </ul>
